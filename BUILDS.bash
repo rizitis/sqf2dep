acccb66dbd638bfff091809a9cf4c84c89e7aefb
rizitis <elahistos@yahoo.gr>
2023-09-17 22:20:37 +0300

    Update BUILDS.bash

--- BUILDS.bash
+++ BUILDS.bash
@@ -19,6 +19,7 @@ process_build_script() {
     BUILD=$(awk -F= '/^BUILD=/{print $2}' "$build_script")
     TAG=$(awk -F= '/^TAG=/{print $2}' "$build_script")
     PKGTYPE=$(awk -F= '/^PKGTYPE=/{print $2}' "$build_script")
+    DEPON="$(cat "$directory"/dependees-on | tr '\n' ' ')"
    
 
     echo "PRGNAM: $PRGNAM" >> "$OUTPUT_FILE"
@@ -26,7 +27,10 @@ process_build_script() {
     echo "BUILD: $BUILD" >> "$OUTPUT_FILE"
     echo "TAG: $TAG" >> "$OUTPUT_FILE"
     echo "PKGTYPE: $PKGTYPE" >> "$OUTPUT_FILE"
+    echo "$PRGNAM"-DEPON: "$DEPON" >> "$OUTPUT_FILE"
     echo "" >> "$OUTPUT_FILE" 
+    
+    
 }
 
 process_directory() {
@@ -38,8 +42,9 @@ process_directory() {
     for slackbuild_file in "$directory"/*.SlackBuild; do
         if [[ -f $slackbuild_file ]]; then
             process_build_script "$slackbuild_file"
-        fi
-    done
+            fi
+  done  
+    
     echo "INFO:" >> "$OUTPUT_FILE"
     cat "$directory/$PRGNAM.info" >> "$OUTPUT_FILE"
     echo " " >> "$OUTPUT_FILE"
